bd6b8d96b3695805f18538d7bb58f63ff7d45c54
sushanthko <sushantk@stud.ntnu.no>
2020-10-04 00:35:02 +0200

    database created.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function allowLoading() { // Source: File API
 
 	const saltSelector = document.getElementById('salt-selector');
   	const ryddeSelector = document.getElementById('rydde-selector');
+  	const mdtSelector = document.getElementById('mdt-selector');
 
 	saltSelector.addEventListener('change', (event) => {
 
@@ -27,6 +28,28 @@ function allowLoading() { // Source: File API
 			}
 		});
 	  });
+
+	mdtSelector.addEventListener('change', (event) => {
+		const fileList = event.target.files;
+
+		Papa.parse(fileList[0], {
+			complete: function (results) {
+				showMDTResultsOnMap(results);
+			}
+		});
+	});
+
+	fetch("data/PlastOPol/marine_debris/database.csv")
+		.then(response => response.text())
+		.then(data => {
+		Papa.parse(data, {
+			complete: function(results) {
+				showDBResultsOnMap(results);
+			}
+		});
+	});
+
+
 }
 
 function showRyddeResultsOnMap(results) {
@@ -203,6 +226,210 @@ function parseWktPoint(stringPoint) {
 	xhttp.send();
 }*/
 
+function showDBResultsOnMap(results) {
+	var greenIcon = new L.Icon({
+		iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+		//shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		//shadowSize: [41, 41]
+	});
 
+	var yellowIcon = new L.Icon({
+		iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png',
+		//shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		//shadowSize: [41, 41]
+	});
+
+	var redIcon = new L.Icon({
+		iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+		//shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		//shadowSize: [41, 41]
+	});
+
+	let allData = results.data;
+
+	for (let i = 1; i < allData.length; ++i) {
+		if (allData[i][4] !== undefined) {
+			let dataString = `Date: ${allData[i][1]}<br>`;
+
+			if(allData[i][4]<50) {
+				L.marker([allData[i][2], allData[i][3]], {icon: greenIcon}).addTo(wasteMap).bindPopup(dataString);
+			}
+			else if(allData[i][4]<200) {
+				L.marker([allData[i][2], allData[i][3]], {icon: yellowIcon}).addTo(wasteMap).bindPopup(dataString);
+			}
+			else {
+				L.marker([allData[i][2], allData[i][3]], {icon: redIcon}).addTo(wasteMap).bindPopup(dataString);
+			}
+		}
+	}
+}
+
+function showMDTResultsOnMap(results) {
+	var yellowIcon = new L.Icon({
+		iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png',
+		//shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		//shadowSize: [41, 41]
+	});
+
+	let allData = results.data;
+
+	let points  = {
+		type: "FeatureCollection",
+		features: []
+	};
+
+	let end = moment().format("YYYY-MM-DD");
+
+	for (let i = 1; i < allData.length; ++i) {
+		if (allData[i][39] === 'NO' && (allData[i][18] !== undefined && allData[i][19] !== undefined)) {
+			// let dataString = `Description: ${allData[i][37]}<br> Location: ${allData[i][26]}<br>`;
+
+			// L.marker([allData[i][18], allData[i][19]], {icon: yellowIcon}).addTo(wasteMap).bindTooltip(dataString);
+			let start = moment(allData[i][28], "DD-MM-YYYY hh:mm").format("YYYY-MM-DD");
+			if (start === "Invalid date") {
+				start = moment(allData[i][28], "MM//DD/YYYY hh:mm").format("YYYY-MM-DD");
+			}
+			console.log(start);
+			let feature = createTimelineFeature("Point", start, end, allData[i][19], allData[i][18]);
+			points.features.push(feature);
+		}
+	}
+	createTimeline(points);
+}
+
+function createTimelineFeature(type, start, end, lng, lat) {
+	return {
+		type: "Feature",
+		properties: {
+			start: start,
+			end: end
+		},
+		geometry: {
+			type: type,
+			coordinates: [lng, lat]
+		}
+	}
+
+}
+
+
+function createTimeline(features) {
+	let pointTimeline = L.timeline(features).addTo(wasteMap);
+
+	let slider = L.timelineSliderControl({
+		formatOutput: function (date) {
+			return moment(date).format("YYYY-MM-DD");
+		},
+	});
+	wasteMap.addControl(slider);
+
+	slider.addTimelines(pointTimeline);
+}
+
+function testTimeline(){
+	let geojsonFeature = {
+		type: "FeatureCollection",
+		features: [
+			{
+				type: "Feature",
+				properties: {
+					start: "1970-01-01",
+					end: "2014-12-04"
+				},
+				geometry: {
+					type: "Point",
+					coordinates: [6.086425781,62.4587092]
+				}
+			},
+			{
+				type: "Feature",
+				properties: {
+					start: "1970-01-01",
+					end: "2015-12-04"
+
+				},
+				geometry: {
+					type: "Point",
+					coordinates: [5.937423706,62.47172371]
+				}
+			},
+			{
+				type: "Feature",
+				properties: {
+					start: "1970-01-01",
+					end: "2016-12-04"
+				},
+				geometry: {
+					type: "Point",
+					coordinates: [6.020507813,62.50851458]
+				}
+			},
+			{
+				type: "Feature",
+				properties: {
+					start: "1970-01-01",
+					end: "2017-12-04"
+				},
+				geometry: {
+					type: "Point",
+					coordinates: [6.096725464,62.57089174]
+				}
+			},
+			{
+				type: "Feature",
+				properties: {
+					start: "1970-01-01",
+					end: "2018-12-04"
+				},
+				geometry: {
+					type: "Point",
+					coordinates: [6.028060913,62.54589324]
+				}
+			},
+			{
+				type: "Feature",
+				properties: {
+					start: "1970-01-01",
+					end: "2018-12-04"
+				},
+				geometry: {
+					type: "Point",
+					coordinates: [6.162643433,62.61166694]
+				}
+			}
+		]
+	};
+
+	let myStyle = {
+		"color": "#ff7800",
+		"weight": 20,
+		"opacity": 0.65
+	};
+
+	let pointTimeline = L.timeline(geojsonFeature, {
+		style: myStyle
+	}).addTo(wasteMap);
+
+	let slider = L.timelineSliderControl({
+		formatOutput: function (date) {
+			return moment(date).format("YYYY-MM-DD");
+		},
+	});
+	wasteMap.addControl(slider);
+
+	slider.addTimelines(pointTimeline);
+};
 
 
