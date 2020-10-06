83adc86a2ed8f634ef035161fcc10528f96fcbb2
sushanthko <sushantk@stud.ntnu.no>
2020-10-06 20:16:26 +0200

    Cell coloring logic based on debris weight added.

--- script.js
+++ script.js
@@ -3,49 +3,49 @@ document.addEventListener("DOMContentLoaded", allowLoading, false);
 
 function allowLoading() { // Source: File API
 
-	const saltSelector = document.getElementById('salt-selector');
-  	const ryddeSelector = document.getElementById('rydde-selector');
-  	const mdtSelector = document.getElementById('mdt-selector');
+	// const saltSelector = document.getElementById('salt-selector');
+  	// const ryddeSelector = document.getElementById('rydde-selector');
+  	// const mdtSelector = document.getElementById('mdt-selector');
+	//
+	// saltSelector.addEventListener('change', (event) => {
+	//
+	//     	const fileList = event.target.files;
+	//
+	// 	Papa.parse(fileList[0], {
+	// 		complete: function(results) {
+	// 			showSALTResultsOnMap(results);
+	// 		}
+	// 	});
+	//   });
+	//
+	// ryddeSelector.addEventListener('change', (event) => {
+	//
+	//     	const fileList = event.target.files;
+	//
+	// 	Papa.parse(fileList[0], {
+	// 		complete: function(results) {
+	// 			showRyddeResultsOnMap(results);
+	// 		}
+	// 	});
+	//   });
+	//
+	// mdtSelector.addEventListener('change', (event) => {
+	// 	const fileList = event.target.files;
+	//
+	// 	Papa.parse(fileList[0], {
+	// 		complete: function (results) {
+	// 			showMDTResultsOnMap(results);
+	// 		}
+	// 	});
+	// });
 
-	saltSelector.addEventListener('change', (event) => {
-
-	    	const fileList = event.target.files;
-
-		Papa.parse(fileList[0], {
-			complete: function(results) {
-				showSALTResultsOnMap(results);
-			}
-		});
-	  });
-
-	ryddeSelector.addEventListener('change', (event) => {
-
-	    	const fileList = event.target.files;
-
-		Papa.parse(fileList[0], {
-			complete: function(results) {
-				showRyddeResultsOnMap(results);
-			}
-		});
-	  });
-
-	mdtSelector.addEventListener('change', (event) => {
-		const fileList = event.target.files;
-
-		Papa.parse(fileList[0], {
-			complete: function (results) {
-				showMDTResultsOnMap(results);
-			}
-		});
-	});
-
-	fetch("data/PlastOPol/marine_debris/database.csv")
+	fetch("data/PlastOPol/database.csv")
 		.then(response => response.text())
 		.then(data => {
 		Papa.parse(data, {
 			complete: function(results) {
 				// showDBResultsOnMap(results);
-				createCells();
+				createCells(results);
 			}
 		});
 	});
@@ -227,31 +227,92 @@ function parseWktPoint(stringPoint) {
 	xhttp.send();
 }*/
 
-const createCells = () => {
-	const lat_min = 62.29277009577185, lat_max = 62.86852390984713, lng_min = 6.06296529018893, lng_max = 7.153935062865635;
+const createCells = results => {
+	// const lat_min = 62.29277009577185, lat_max = 62.86852390984713, lng_min = 6.06296529018893, lng_max = 7.153935062865635; //Alesund
+	const lat_min = 67.775, lat_max = 69.35, lng_min = 12.5, lng_max = 16.15; // Lofoten
+	let allData = results.data;
 
-	const cell_rows = 10, cell_columns = 5;
+	const cell_rows = 28, cell_columns = 28;
 
 	const half_cell_height = (lat_max - lat_min)/(2*(cell_rows - 1));
 	const half_cell_width = (lng_max - lng_min)/(2*(cell_columns - 1));
 
 	let lat_point = lat_min;
 	let row_count = 0;
+	let quantity_max = null;
+	let quantity_min = null;
+	let weight_max = null;
+	let weight_min = null;
+	let cells = [];
 	while (row_count < cell_rows){
 		let lng_point = lng_min;
 		let column_count = 0;
+		let inner_cells = [];
 		while (column_count < cell_columns) {
-			var bounds = [[lat_point - half_cell_height, lng_point - half_cell_width],
-				[lat_point + half_cell_height, lng_point + half_cell_width]];
-			// create an orange rectangle
-			L.rectangle(bounds, {color: "#ff7800", weight: 1}).addTo(wasteMap);
+			let lat_0 = lat_point - half_cell_height;
+			let lng_0 = lng_point - half_cell_width;
+			let lat_1 = lat_point + half_cell_height;
+			let lng_1 = lng_point + half_cell_width;
+			let bounds = [[lat_0, lng_0], [lat_1, lng_1]];
+
+			let total_quantity = 0;
+			let total_weight = 0;
+			for (let i = 1; i < allData.length; ++i) {
+				if(i<931 && i>2094){
+					continue;
+				}
+					let lat_data = allData[i][2];
+					let lng_data = allData[i][3];
+					if(lat_data > lat_0 && lat_data <= lat_1 && lng_data > lng_0 && lng_data <= lng_1) {
+						let quantity_data = allData[i][4];
+						let weight_data = allData[i][5];
+						if (quantity_data.trim() !== "") {
+							total_quantity += parseInt(quantity_data);
+						}
+						if (weight_data.trim() !== "") {
+							total_weight += parseFloat(weight_data);
+						}
+					}
+			}
+			if (quantity_max === null || total_quantity > quantity_max){
+				quantity_max = total_quantity;
+			}
+			if (quantity_min === null || total_quantity < quantity_min){
+				quantity_min = total_quantity;
+			}
+
+			if (weight_max === null || total_weight > weight_max){
+				weight_max = total_weight;
+			}
+			if (weight_min === null || total_weight < weight_min){
+				weight_min = total_weight;
+			}
+			inner_cells[column_count] = {bounds: bounds, quantity: total_quantity, weight: total_weight};
 			lng_point += 2*half_cell_width;
 			column_count++;
 		}
+		cells[row_count] = inner_cells;
 		lat_point += 2*half_cell_height;
 		row_count++;
 	}
+
+	for(i=0; i<cells.length; i++){
+		let inner_cells =  cells[i];
+		for(j=0; j<inner_cells.length; j++){
+			let cell = inner_cells[j];
+			let value = normalized_rgb(cell.weight, weight_max, weight_min)
+
+			let dataString = `Weight: ${cell.weight} kg`;
+			// create a grayscale rectangle
+			L.rectangle(cell.bounds, {color: "rgb("+value+", "+value+", "+value+")", weight: 0, fillOpacity: 0.8}).addTo(wasteMap).bindPopup(dataString);
+		}
+	}
+	wasteMap.fitBounds([[lat_min, lng_min], [lat_max, lng_max]]);
 };
+
+function normalized_rgb(old_val, max, min){
+	return 255 - (old_val - min)*(255/(max-min));
+}
 
 function showDBResultsOnMap(results) {
 	var greenIcon = new L.Icon({
@@ -285,7 +346,7 @@ function showDBResultsOnMap(results) {
 
 	for (let i = 1; i < allData.length; ++i) {
 		if (allData[i][4] !== undefined) {
-			let dataString = `Date: ${allData[i][1]}<br> Lat: ${allData[i][2]}<br> Lng: ${allData[i][3]}<br>`;
+			let dataString = `Date: ${allData[i][1]}<br> Lat: ${allData[i][2]}<br> Lng: ${allData[i][3]}<br> Qt: ${allData[i][4]}<br>`;
 
 			if(allData[i][4]<50) {
 				L.marker([allData[i][2], allData[i][3]], {icon: greenIcon}).addTo(wasteMap).bindPopup(dataString);
